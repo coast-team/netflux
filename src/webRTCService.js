c950f37a6429203186a3d5537c6018027ad80e6d
Philippe Kalitine <philippe.kalitine@gmail.com>
2016-01-25 18:55:07 +0100

    fix(webRTCService & package.json): implement webRTCService + tests
    First implementation of webRTCService (open & join methods). Add test for this class. Add
    sigver(signalling server) as devdependency for tests.

--- src/webRTCService.js
+++ src/webRTCService.js
@@ -1,5 +1,23 @@
 class WebRTCService {
-  constructor () {
+
+  constructor (options = {}) {
+    this.defaults = {
+      signalling: '',
+      webRTCOptions: {
+        iceServers: [{
+          urls: ['stun:23.21.150.121', 'stun:stun.l.google.com:19302']
+        }, {
+          urls: 'turn:numb.viagenie.ca',
+          credential: 'webrtcdemo',
+          username: 'louis%40mozilla.com'
+        }]
+      }
+    }
+    this.settings = Object.assign({}, this.defaults, options)
+    this.socket = null
+    this.me = null
+
+    // Declare WebRTC related global(window) constructors
     this.RTCPeerConnection =
       window.RTCPeerConnection ||
       window.mozRTCPeerConnection ||
@@ -17,38 +35,89 @@ class WebRTCService {
       window.mozRTCSessionDescription ||
       window.webkitRTCSessionDescription ||
       window.msRTCSessionDescription
-
-    this.default = {
-      iceServers: [{
-        urls: 'stun:23.21.150.121'
-      }, {
-        urls: 'stun:stun.l.google.com:19302'
-      }, {
-        urls: 'turn:numb.viagenie.ca',
-        credential: 'webrtcdemo',
-        username: 'louis%40mozilla.com'
-      }]
-    }
   }
 
-  connect (signalingServerURL) {
-    // let peer = new Peer();
-    // let me = new this.RTCPeerConnection(this.default);
-    // let dataChannel = me.createDataChannel(this._randomString());
-    // let promise = new Promise((resolve, reject) => {
-    //
-    // });
+  open (onchannel, options) {
+    let settings = Object.assign({}, this.settings, {key: this._randomKey()}, options)
+    return new Promise((resolve, reject) => {
+      try {
+        this.socket = new window.WebSocket(settings.signalling)
+        this.socket.onerror = (e) => { reject(e) }
+        this.socket.onopen = () => {
+          this.socket.send(JSON.stringify({key: settings.key}))
+          this.me = new this.RTCPeerConnection(settings.webRTCOptions)
+          this._setupIceGathering(this.me, this.socket, false)
+          this.me.ondatachannel = (e) => {
+            e.channel.onopen = () => {
+              onchannel(e.channel)
+            }
+          }
+          resolve({url: this.settings.signalling, key: settings.key})
+        }
+        this.socket.onmessage = (e) => {
+          let msg = JSON.parse(e.data)
+          if (msg.offer.hasOwnProperty('candidate')) {
+            let candidate = new this.RTCIceCandidate(msg.offer.candidate)
+            this.me.addIceCandidate(candidate, () => {}, (e) => {
+              reject(e)
+            })
+          } else {
+            let sd = Object.assign(new this.RTCSessionDescription(), msg.offer)
+            this.me.setRemoteDescription(sd, () => {
+              this.me.createAnswer((answer) => {
+                this.me.setLocalDescription(answer, () => {
+                  this.socket.send(JSON.stringify({answer: this.me.localDescription.toJSON()}))
+                }, (e) => { reject(e) })
+              }, (e) => {
+                reject(e)
+              })
+            }, (e) => {
+              reject(e)
+            })
+          }
+        }
+      } catch (e) { reject(e) }
+    })
   }
 
-  disconnect (connectorObj) {
-
+  join (joinkey, options) {
+    let settings = Object.assign({}, this.settings, options)
+    return new Promise((resolve, reject) => {
+      try {
+        let socket = new window.WebSocket(settings.signalling)
+        socket.onerror = (e) => { reject(e) }
+        socket.onopen = () => {
+          socket.send(JSON.stringify({joinkey}))
+          this.me = new this.RTCPeerConnection(settings.webRTCOptions)
+          this._setupIceGathering(this.me, socket, true)
+          let dc = this.me.createDataChannel(joinkey)
+          dc.onopen = () => { resolve(dc) }
+        }
+        socket.onmessage = (e) => {
+          let msg = JSON.parse(e.data)
+          if (msg.hasOwnProperty('reachable')) {
+            this.me.createOffer((offer) => {
+              this.me.setLocalDescription(offer, () => {
+                socket.send(JSON.stringify({offer: this.me.localDescription.toJSON()}))
+              }, (e) => { reject(e) })
+            }, (e) => { reject(e) })
+          } else if (msg.answer.hasOwnProperty('candidate')) {
+            let candidate = new this.RTCIceCandidate(msg.answer.candidate)
+            this.me.addIceCandidate(candidate, () => {}, (e) => {
+              reject(e)
+            })
+          } else {
+            let sd = Object.assign(new this.RTCSessionDescription(), msg.answer)
+            this.me.setRemoteDescription(sd, () => {}, (e) => {
+              reject(e)
+            })
+          }
+        }
+      } catch (e) { reject(e) }
+    })
   }
 
-  initiateConnection () {
-
-  }
-
-  finalizeConnection (anotherPeerSDPData) {
+  disableToConnect (anotherPeerSDPData) {
 
   }
 
@@ -60,7 +129,23 @@ class WebRTCService {
 
   }
 
-  _randomString () {
+  _setupIceGathering (rtcpc, socket, isOffer) {
+    rtcpc.onicecandidate = (e) => {
+      if (e.candidate !== null) {
+        let candidate = {
+          candidate: e.candidate.candidate,
+          sdpMLineIndex: e.candidate.sdpMLineIndex
+        }
+        if (isOffer) {
+          socket.send(JSON.stringify({offer: {candidate}}))
+        } else {
+          socket.send(JSON.stringify({answer: {candidate}}))
+        }
+      }
+    }
+  }
+
+  _randomKey () {
     const MIN_LENGTH = 10
     const DELTA_LENGTH = 10
     const MASK = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
--- test/webRTCService.test.js
+++ test/webRTCService.test.js
@@ -1,16 +1,180 @@
 import { WebRTCService } from '../src/webRTCService.js'
 
-describe('webRTCService', () => {
-  const webRTCService = new WebRTCService()
+const signalling = 'ws://localhost:8000'
 
-  describe('_randomString', () => {
-    it('Two random strings should be different', () => {
-      const TEST_COUNTER = 10
+describe('WebRTCService ->', () => {
+  describe('_randomKey', () => {
+    it('two random strings should be different', () => {
+      let webRTCService = new WebRTCService({signalling})
+      let TEST_COUNTER = 10
       for (let i = 0; i < TEST_COUNTER; i++) {
-        const str1 = webRTCService._randomString()
-        const str2 = webRTCService._randomString()
+        let str1 = webRTCService._randomKey()
+        let str2 = webRTCService._randomKey()
         expect(str1).not.toBe(str2)
       }
+    })
+  })
+
+  describe('Connection between two peers via dataChannel ->', () => {
+    let webRTCService1, webRTCService2
+    beforeEach(() => {
+      webRTCService1 = new WebRTCService({signalling})
+      webRTCService2 = new WebRTCService({signalling})
+    })
+
+    describe('open', () => {
+      it('should return an URL', (done) => {
+        webRTCService1
+          .open(() => {})
+          .then((data) => {
+            expect(true).toBeTruthy()
+            done()
+          })
+          .catch((e) => {
+            expect(true).toBeFalsy()
+            done.fail(e)
+          })
+      })
+
+      it('should receive a dataChannel with specified key', (done) => {
+        let label
+        webRTCService1
+          .open((dc) => {
+            expect(dc.label).toBe(label)
+            done()
+          })
+          .then((data) => {
+            label = data.key
+            webRTCService2.join(data.key)
+          })
+      })
+
+      it('with wrong url: catch - should throw an exception', (done) => {
+        webRTCService1 = new WebRTCService({
+          signalling: 'ws://localhost:8100'
+        })
+        webRTCService1
+          .open(() => {})
+          .then((data) => {
+            expect(true).toBeFalsy()
+            done.fail()
+          })
+          .catch((e) => {
+            expect(true).toBeTruthy()
+            done()
+          })
+      })
+
+      it('with incorrect url syntax: catch - should throw DOMException', (done) => {
+        webRTCService1 = new WebRTCService({
+          signalling: 'https://github.com:8100/coast-team/netflux'
+        })
+        webRTCService1
+          .open(() => {})
+          .then((data) => {
+            expect(true).toBeFalsy()
+            done.fail()
+          })
+          .catch((e) => {
+            expect(e instanceof DOMException).toBeTruthy()
+            expect(e.name).toBe('SyntaxError')
+            expect(e.code).toBe(12)
+            done()
+          })
+      })
+    })
+
+    describe('join', () => {
+      it('dataChannel should be open', (done) => {
+        webRTCService1
+          .open(() => {})
+          .then((data) => {
+            webRTCService2
+              .join(data.key)
+              .then((dc) => {
+                expect(true).toBeTruthy()
+                done()
+              })
+              .catch((e) => {
+                done.fail(e)
+              })
+          })
+          .catch((e) => {
+            done.fail(e)
+          })
+      })
+
+      it('with wrong url: catch - should throw an exception', (done) => {
+        webRTCService2 = new WebRTCService({
+          signalling: 'https://github.com:8100/coast-team/netflux'
+        })
+        webRTCService2
+          .join('some key')
+          .then((data) => {
+            expect(true).toBeFalsy()
+            done.fail()
+          })
+          .catch((e) => {
+            expect(true).toBeTruthy()
+            done()
+          })
+      })
+
+      it('with incorrect url syntax: catch - should throw DOMException', (done) => {
+        webRTCService2 = new WebRTCService({
+          signalling: 'https://github.com:8100/coast-team/netflux'
+        })
+        webRTCService2
+          .join('some key')
+          .then((data) => {
+            expect(true).toBeFalsy()
+            done.fail()
+          })
+          .catch((e) => {
+            expect(e instanceof DOMException).toBeTruthy()
+            expect(e.name).toBe('SyntaxError')
+            expect(e.code).toBe(12)
+            done()
+          })
+      })
+    })
+
+    describe('open & join', () => {
+      it('Message should be sent and received via dataChannel', (done) => {
+        let peer1Msg = 'Hello! Here is peer #1'
+        let peer2Msg = 'Hi, I am peer #2'
+        webRTCService1
+          .open((dc) => {
+            dc.send(peer1Msg)
+            dc.onmessage = (event) => {
+              expect(event.data).toEqual(peer2Msg)
+              dc.close()
+            }
+            dc.onclose = (event) => {
+              expect(true).toBeTruthy()
+            }
+          })
+          .then((data) => {
+            webRTCService2
+              .join(data.key)
+              .then((dc) => {
+                dc.onmessage = (event) => {
+                  expect(event.data).toEqual(peer1Msg)
+                  dc.send(peer2Msg)
+                }
+                dc.onclose = (event) => {
+                  expect(true).toBeTruthy()
+                  done()
+                }
+              })
+              .catch((e) => {
+                done.fail(e)
+              })
+          })
+          .catch((e) => {
+            done.fail(e)
+          })
+      })
     })
   })
 })
