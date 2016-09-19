ff04934344d4df8bda69b7e10b470923f56bbb3e
Philippe Kalitine <philippe.kalitine@gmail.com>
2016-09-19 11:42:41 +0200

    chore(devDependencies): update pm2 and rollup

--- dist/netflux.es2015.umd.js
+++ dist/netflux.es2015.umd.js
@@ -1738,10 +1738,6 @@ const WebSocket = isBrowser() ? window.WebSocket : require('ws')
 const CONNECT_TIMEOUT$1 = 7000
 const OPEN = WebSocket.OPEN
 let listenOnWebSocket = false
-let setListenOnWebSocket = (value) => {
-  listenOnWebSocket = value
-}
-
 class WebSocketService extends ServiceInterface {
 
   /**
@@ -3204,10 +3200,10 @@ class Bot {
     return null
   }
 
-  leave (WebChannel) {
+  leave (WebChannel$$1) {
     let index = -1
     for (let i = 0; i < this.webChannels.length; i++) {
-      if (WebChannel.id === this.webChannels[i].id) {
+      if (WebChannel$$1.id === this.webChannels[i].id) {
         index = i
         break
       }
@@ -3223,6 +3219,103 @@ class Bot {
     }
   }
 }
+
+/**
+ * @external JSON
+ * @see {@link https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/JSON}
+ */
+/**
+ * @external Error
+ * @see {@link https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/Error}
+ */
+/**
+ * @external RTCPeerConnection
+ * @see {@link https://developer.mozilla.org/en/docs/Web/API/RTCPeerConnection}
+ */
+/**
+ * @external RTCSessionDescription
+ * @see {@link https://developer.mozilla.org/en/docs/Web/API/RTCSessionDescription}
+ */
+/**
+ * @external RTCDataChannel
+ * @see {@link https://developer.mozilla.org/en/docs/Web/API/RTCDataChannel}
+ */
+/**
+ * @external RTCIceCandidate
+ * @see {@link https://developer.mozilla.org/en/docs/Web/API/RTCIceCandidate}
+ */
+/**
+ * @external RTCPeerConnectionIceEvent
+ * @see {@link https://developer.mozilla.org/en/docs/Web/API/RTCPeerConnectionIceEvent}
+ */
+/**
+ * @external CloseEvent
+ * @see {@link https://developer.mozilla.org/en/docs/Web/API/CloseEvent/CloseEvent}
+ */
+/**
+ * @external WebSocket
+ * @see {@link https://developer.mozilla.org/en/docs/Web/API/WebSocket}
+ */
+/**
+ * @external Set
+ * @see {@link https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/Set}
+ */
+/**
+ * @external Map
+ * @see {@link https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/Map}
+ */
+/**
+ * @external ArrayBufferView
+ * @see {@link https://developer.mozilla.org/en/docs/Web/API/ArrayBufferView}
+ */
+/**
+ * @external ArrayBuffer
+ * @see {@link https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/ArrayBuffer}
+ */
+/**
+ * @external String
+ * @see {@link https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/String}
+ */
+/**
+ * @external Int8Array
+ * @see {@link developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/Int8Array}
+ */
+/**
+ * @external Uint8Array
+ * @see {@link developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/Uint8Array}
+ */
+/**
+ * @external Uint8ClampedArray
+ * @see {@link https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/Uint8ClampedArray}
+ */
+/**
+ * @external Int16Array
+ * @see {@link https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/Int16Array}
+ */
+/**
+ * @external Uint16Array
+ * @see {@link https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/Uint16Array}
+ */
+/**
+ * @external Int32Array
+ * @see {@link https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/Int32Array}
+ */
+/**
+ * @external Uint32Array
+ * @see {@link https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/Uint32Array}
+ */
+/**
+ * @external Float32Array
+ * @see {@link https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/Float32Array}
+ */
+/**
+ * @external Float64Array
+ * @see {@link https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/Float64Array}
+ */
+/**
+ * @external DataView
+ * @see {@link https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/DataView}
+ */
 
 exports.WebChannel = WebChannel;
 exports.Bot = Bot;
