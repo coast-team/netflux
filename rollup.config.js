6d249bdf4684ca52a630401640b985ca5a3250cb
Philippe Kalitine <philippe.kalitine@gmail.com>
2017-07-25 17:13:02 +0200

    build(rollup): fix browser build

--- rollup.config.js
+++ rollup.config.js
@@ -3,23 +3,8 @@ import babel from 'rollup-plugin-babel'
 import strip from 'rollup-plugin-strip'
 import commonjs from 'rollup-plugin-commonjs'
 import resolve from 'rollup-plugin-node-resolve'
+import replace from 'rollup-plugin-re'
 import pkg from './package.json'
-
-const plugins = [
-  strip({
-    functions: [ 'log.info', 'log.debug', 'log.trace', 'log.goupe' ]
-  }),
-  resolve(),
-  commonjs({
-    namedExports: {
-      'node_modules/protobufjs/minimal.js': [ 'Reader', 'Writer', 'util', 'roots' ]
-    }
-  }),
-  babel({
-    exclude: 'node_modules/**'
-  }),
-  filesize({ format: { round: 0 } })
-]
 
 export default [
   {
@@ -29,13 +14,51 @@ export default [
       { dest: pkg.module, format: 'es' }
     ],
     external: ['wrtc', 'uws', 'text-encoding'],
-    plugins
+    plugins: [
+      strip({
+        functions: [ 'console.info', 'console.log', 'console.trace', 'console.goupe' ]
+      }),
+      replace({
+        defines: {
+          NODE: true
+        }
+      }),
+      resolve(),
+      commonjs({
+        namedExports: {
+          'node_modules/protobufjs/minimal.js': [ 'Reader', 'Writer', 'util', 'roots' ]
+        }
+      }),
+      babel({
+        exclude: 'node_modules/**'
+      }),
+      filesize({ format: { round: 0 } })
+    ]
   },
   {
     entry: 'src/index.js',
     format: 'umd',
     moduleName: 'netflux',
     dest: pkg.browser,
-    plugins
+    plugins: [
+      strip({
+        functions: [ 'console.info', 'console.log', 'console.trace', 'console.goupe' ]
+      }),
+      replace({
+        defines: {
+          NODE: false
+        }
+      }),
+      resolve(),
+      commonjs({
+        namedExports: {
+          'node_modules/protobufjs/minimal.js': [ 'Reader', 'Writer', 'util', 'roots' ]
+        }
+      }),
+      babel({
+        exclude: 'node_modules/**'
+      }),
+      filesize({ format: { round: 0 } })
+    ]
   }
 ]
