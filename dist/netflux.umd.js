5fe5f5c1f9ac43e34336af0f5b0624baaa696ee7
Philippe Kalitine <philippe.kalitine@gmail.com>
2017-07-28 13:33:48 +0200

    refactor(UserMessage): convert UserMessage.js into typescript

--- dist/netflux.umd.js
+++ dist/netflux.umd.js
@@ -8911,183 +8911,155 @@ class ChannelBuilder extends Service$1 {
 
 /**
  * Maximum size of the user message sent over `Channel`. Is meant without metadata.
- * @type {number}
  */
-const MAX_USER_MSG_SIZE = 15000;
-
+var MAX_USER_MSG_SIZE = 15000;
 /**
  * Maximum message id number.
- * @type {number}
  */
-const MAX_MSG_ID_SIZE = 65535;
-
-const textEncoder = new _TextEncoder();
-const textDecoder = new _TextDecoder();
-
+var MAX_MSG_ID_SIZE = 65535;
+var textEncoder = new _TextEncoder();
+var textDecoder = new _TextDecoder();
 /**
  * Message builder service is responsible to build messages to send them over the
  * `WebChannel` and treat messages received by the `WebChannel`. It also manage
  * big messages (more then 16ko) sent by users. Internal messages are always less
  * 16ko.
  */
-class UserMessage {
-  constructor () {
-    this.buffers = new Map();
-  }
-
-  /**
-   * @callback MessageService~Send
-   * @param {ArrayBuffer} dataChunk - If the message is too big this
-   * action would be executed for each data chunk until send whole message
-   */
-
-  /**
-   * @private
-   * @typedef {ARRAY_BUFFER_TYPE|STRING_TYPE} MessageTypeEnum
-   */
-
-  /**
-   * Prepare user message to be sent over the `WebChannel`.
-   *
-   * @param {UserMessage} data Message to be sent
-   * @param {number} senderId Id of the peer who sends this message
-   * @param {number} recipientId Id of the recipient peer
-   * @param {boolean} [isBroadcast=true] Equals to true if this message would be
-   * sent to all `WebChannel` members and false if only to one member
-   * @return {ArrayBuffer}
-   */
-  encode (data) {
-    const {type, bytes} = this.userDataToType(data);
-    const msg = { length: bytes.length, type };
-    if (bytes.length <= MAX_USER_MSG_SIZE) {
-      msg.full = bytes;
-      return [user.Message.encode(user.Message.create(msg)).finish()]
-    } else {
-      msg.chunk = { id: Math.ceil(Math.random() * MAX_MSG_ID_SIZE) };
-      const numberOfChunks = Math.ceil(bytes.length / MAX_USER_MSG_SIZE);
-      const res = new Array(numberOfChunks);
-      for (let number = 0; number < numberOfChunks; number++) {
-        const length = Math.min(
-          MAX_USER_MSG_SIZE,
-          bytes.length - MAX_USER_MSG_SIZE * number
-        );
-        const begin = MAX_USER_MSG_SIZE * number;
-        const end = begin + length;
-        msg.chunk.number = number;
-        msg.chunk.content = new Uint8Array(bytes.slice(begin, end));
-        res[number] = user.Message.encode(user.Message.create(msg)).finish();
-      }
-      return res
+var UserMessage = (function () {
+    function UserMessage() {
+        this.buffers = new Map();
     }
-  }
-
-  decode (bytes, senderId) {
-    const msg = user.Message.decode(bytes);
-    let content;
-    switch (msg.content) {
-      case 'full': {
-        content = msg.full;
-        break
-      }
-      case 'chunk': {
-        let buffer = this.getBuffer(senderId, msg.chunk.id);
-        if (buffer === undefined) {
-          buffer = new Buffer$1(msg.length, msg.chunk.content, msg.chunk.number);
-          this.setBuffer(senderId, msg.chunk.id, buffer);
-          content = undefined;
-        } else {
-          content = buffer.append(msg.chunk.content, msg.chunk.number);
+    /**
+     * Encode user message for sending over the network.
+     */
+    UserMessage.prototype.encode = function (data) {
+        var _a = this.userDataToType(data), type = _a.type, bytes = _a.bytes;
+        var msg = { length: bytes.length, type: type };
+        if (bytes.length <= MAX_USER_MSG_SIZE) {
+            msg.full = bytes;
+            return [user.Message.encode(user.Message.create(msg)).finish()];
         }
-        break
-      }
-      default: {
-        throw new Error('Unknown message integrity')
-      }
-    }
-    if (content !== undefined) {
-      switch (msg.type) {
-        case user.Message.Type.U_INT_8_ARRAY:
-          return content
-        case user.Message.Type.STRING:
-          return textDecoder.decode(content)
-        default:
-          throw new Error('Unknown message type')
-      }
-    }
-    return content
-  }
-
-  /**
-   * Identify the user message type.
-   *
-   * @private
-   * @param {Message} data User message
-   * @returns {MessageTypeEnum} User message type
-   */
-  userDataToType (data) {
-    if (data instanceof Uint8Array) {
-      return { type: user.Message.Type.U_INT_8_ARRAY, bytes: data }
-    } else if (typeof data === 'string' || data instanceof String) {
-      return { type: user.Message.Type.STRING, bytes: textEncoder.encode(data) }
-    } else {
-      throw new Error('Message neigther a string or a Uint8Array object')
-    }
-  }
-
-  getBuffer (peerId, msgId) {
-    const buffers = this.buffers.get(peerId);
-    if (buffers !== undefined) {
-      return buffers.get(msgId)
-    }
-    return undefined
-  }
-
-  setBuffer (peerId, msgId, buffer) {
-    let buffers = this.buffers.get(peerId);
-    if (buffers === undefined) {
-      buffers = new Map();
-    }
-    buffers.set(msgId, buffer);
-    this.buffers.set(peerId, buffers);
-  }
-}
-
+        else {
+            msg.chunk = { id: Math.ceil(Math.random() * MAX_MSG_ID_SIZE) };
+            var numberOfChunks = Math.ceil(bytes.length / MAX_USER_MSG_SIZE);
+            var res = new Array(numberOfChunks);
+            for (var i = 0; i < numberOfChunks; i++) {
+                var length_1 = Math.min(MAX_USER_MSG_SIZE, bytes.length - MAX_USER_MSG_SIZE * i);
+                var begin = MAX_USER_MSG_SIZE * i;
+                var end = begin + length_1;
+                msg.chunk.number = i;
+                msg.chunk.content = new Uint8Array(bytes.slice(begin, end));
+                res[i] = user.Message.encode(user.Message.create(msg)).finish();
+            }
+            return res;
+        }
+    };
+    /**
+     * Decode user message received from the network.
+     */
+    UserMessage.prototype.decode = function (bytes, senderId) {
+        var msg = user.Message.decode(bytes);
+        var content;
+        switch (msg.content) {
+            case 'full': {
+                content = msg.full;
+                break;
+            }
+            case 'chunk': {
+                var buffer = this.getBuffer(senderId, msg.chunk.id);
+                if (buffer === undefined) {
+                    buffer = new Buffer$1(msg.length, msg.chunk.content, msg.chunk.number);
+                    this.setBuffer(senderId, msg.chunk.id, buffer);
+                    content = undefined;
+                }
+                else {
+                    content = buffer.append(msg.chunk.content, msg.chunk.number);
+                }
+                break;
+            }
+            default: {
+                throw new Error('Unknown message integrity');
+            }
+        }
+        if (content !== undefined) {
+            switch (msg.type) {
+                case user.Message.Type.U_INT_8_ARRAY:
+                    return content;
+                case user.Message.Type.STRING:
+                    return textDecoder.decode(content);
+                default:
+                    throw new Error('Unknown message type');
+            }
+        }
+        return content;
+    };
+    /**
+     * Identify the user data type.
+     */
+    UserMessage.prototype.userDataToType = function (data) {
+        if (data instanceof Uint8Array) {
+            return { type: user.Message.Type.U_INT_8_ARRAY, bytes: data };
+        }
+        else if (typeof data === 'string' || data instanceof String) {
+            return { type: user.Message.Type.STRING, bytes: textEncoder.encode(data) };
+        }
+        else {
+            throw new Error('Message neigther a string type or a Uint8Array type');
+        }
+    };
+    UserMessage.prototype.getBuffer = function (peerId, msgId) {
+        var buffers = this.buffers.get(peerId);
+        if (buffers !== undefined) {
+            return buffers.get(msgId);
+        }
+        return undefined;
+    };
+    UserMessage.prototype.setBuffer = function (peerId, msgId, buffer) {
+        var buffers = this.buffers.get(peerId);
+        if (buffers === undefined) {
+            buffers = new Map();
+        }
+        buffers.set(msgId, buffer);
+        this.buffers.set(peerId, buffers);
+    };
+    return UserMessage;
+}());
 /**
  * Buffer class used when the user message exceeds the message size limit which
  * may be sent over a `Channel`. Each buffer is identified by `WebChannel` id,
  * peer id (who sends the big message) and message id (in case if the peer sends
  * more then 1 big message at a time).
- * @private
  */
-class Buffer$1 {
-  /**
-   * @param {number} totalLength The total user message size
-   * @param {ArrayBuffer} data The first chunk of the user message
-   * @param {number} chunkNb Number of the chunk
-   * @param {function(buffer: ArrayBuffer)} action Callback to be executed when all
-   * message chunks are received and thus the message is ready
-   */
-  constructor (totalLength, data, chunkNb) {
-    this.fullData = new Uint8Array(totalLength);
-    this.currentLength = 0;
-    this.append(data, chunkNb);
-  }
-
-  /**
-   * Add a chunk of message to the buffer.
-   * @param {ArrayBuffer} data - Message chunk
-   * @param {number} chunkNb - Number of the chunk
-   * @return {undefined|ArrayBuffer}
-   */
-  append (data, chunkNb) {
-    let i = chunkNb * MAX_USER_MSG_SIZE;
-    this.currentLength += data.length;
-    for (let d of data) {
-      this.fullData[i++] = d;
+var Buffer$1 = (function () {
+    function Buffer(totalLength, data, chunkNb) {
+        this.fullData = new Uint8Array(totalLength);
+        this.currentLength = 0;
+        this.append(data, chunkNb);
     }
-    return this.currentLength === this.fullData.length ? this.fullData : undefined
-  }
-}
+    /**
+     * Add a chunk of message to the buffer.
+     */
+    Buffer.prototype.append = function (data, chunkNb) {
+        var i = chunkNb * MAX_USER_MSG_SIZE;
+        this.currentLength += data.length;
+        try {
+            for (var data_1 = __values(data), data_1_1 = data_1.next(); !data_1_1.done; data_1_1 = data_1.next()) {
+                var d = data_1_1.value;
+                this.fullData[i++] = d;
+            }
+        }
+        catch (e_1_1) { e_1 = { error: e_1_1 }; }
+        finally {
+            try {
+                if (data_1_1 && !data_1_1.done && (_a = data_1.return)) _a.call(data_1);
+            }
+            finally { if (e_1) throw e_1.error; }
+        }
+        return this.currentLength === this.fullData.length ? this.fullData : undefined;
+        var e_1, _a;
+    };
+    return Buffer;
+}());
 
 /**
  * @type {Object}
--- src/UserMessage.ts
+++ src/UserMessage.ts
@@ -3,18 +3,18 @@ import { TextEncoder, TextDecoder } from './polyfills'
 
 /**
  * Maximum size of the user message sent over `Channel`. Is meant without metadata.
- * @type {number}
  */
 const MAX_USER_MSG_SIZE = 15000
 
 /**
  * Maximum message id number.
- * @type {number}
  */
 const MAX_MSG_ID_SIZE = 65535
 
 const textEncoder = new TextEncoder()
 const textDecoder = new TextDecoder()
+
+type UserDataType = Uint8Array | string
 
 /**
  * Message builder service is responsible to build messages to send them over the
@@ -23,34 +23,20 @@ const textDecoder = new TextDecoder()
  * 16ko.
  */
 export class UserMessage {
+
+  private buffers: Map<number, Map<number, Buffer>>
+
   constructor () {
     this.buffers = new Map()
   }
 
-  /**
-   * @callback MessageService~Send
-   * @param {ArrayBuffer} dataChunk - If the message is too big this
-   * action would be executed for each data chunk until send whole message
-   */
 
   /**
-   * @private
-   * @typedef {ARRAY_BUFFER_TYPE|STRING_TYPE} MessageTypeEnum
+   * Encode user message for sending over the network.
    */
-
-  /**
-   * Prepare user message to be sent over the `WebChannel`.
-   *
-   * @param {UserMessage} data Message to be sent
-   * @param {number} senderId Id of the peer who sends this message
-   * @param {number} recipientId Id of the recipient peer
-   * @param {boolean} [isBroadcast=true] Equals to true if this message would be
-   * sent to all `WebChannel` members and false if only to one member
-   * @return {ArrayBuffer}
-   */
-  encode (data) {
+  encode (data): Uint8Array[] {
     const {type, bytes} = this.userDataToType(data)
-    const msg = { length: bytes.length, type }
+    const msg: any = { length: bytes.length, type }
     if (bytes.length <= MAX_USER_MSG_SIZE) {
       msg.full = bytes
       return [user.Message.encode(user.Message.create(msg)).finish()]
@@ -58,75 +44,74 @@ export class UserMessage {
       msg.chunk = { id: Math.ceil(Math.random() * MAX_MSG_ID_SIZE) }
       const numberOfChunks = Math.ceil(bytes.length / MAX_USER_MSG_SIZE)
       const res = new Array(numberOfChunks)
-      for (let number = 0; number < numberOfChunks; number++) {
+      for (let i = 0; i < numberOfChunks; i++) {
         const length = Math.min(
           MAX_USER_MSG_SIZE,
-          bytes.length - MAX_USER_MSG_SIZE * number
+          bytes.length - MAX_USER_MSG_SIZE * i
         )
-        const begin = MAX_USER_MSG_SIZE * number
+        const begin = MAX_USER_MSG_SIZE * i
         const end = begin + length
-        msg.chunk.number = number
+        msg.chunk.number = i
         msg.chunk.content = new Uint8Array(bytes.slice(begin, end))
-        res[number] = user.Message.encode(user.Message.create(msg)).finish()
+        res[i] = user.Message.encode(user.Message.create(msg)).finish()
       }
       return res
     }
   }
 
-  decode (bytes, senderId) {
+  /**
+   * Decode user message received from the network.
+   */
+  decode (bytes: Uint8Array, senderId: number): UserDataType {
     const msg = user.Message.decode(bytes)
     let content
     switch (msg.content) {
-      case 'full': {
-        content = msg.full
-        break
+    case 'full': {
+      content = msg.full
+      break
+    }
+    case 'chunk': {
+      let buffer = this.getBuffer(senderId, msg.chunk.id)
+      if (buffer === undefined) {
+        buffer = new Buffer(msg.length, msg.chunk.content, msg.chunk.number)
+        this.setBuffer(senderId, msg.chunk.id, buffer)
+        content = undefined
+      } else {
+        content = buffer.append(msg.chunk.content, msg.chunk.number)
       }
-      case 'chunk': {
-        let buffer = this.getBuffer(senderId, msg.chunk.id)
-        if (buffer === undefined) {
-          buffer = new Buffer(msg.length, msg.chunk.content, msg.chunk.number)
-          this.setBuffer(senderId, msg.chunk.id, buffer)
-          content = undefined
-        } else {
-          content = buffer.append(msg.chunk.content, msg.chunk.number)
-        }
-        break
-      }
-      default: {
-        throw new Error('Unknown message integrity')
-      }
+      break
+    }
+    default: {
+      throw new Error('Unknown message integrity')
+    }
     }
     if (content !== undefined) {
       switch (msg.type) {
-        case user.Message.Type.U_INT_8_ARRAY:
-          return content
-        case user.Message.Type.STRING:
-          return textDecoder.decode(content)
-        default:
-          throw new Error('Unknown message type')
+      case user.Message.Type.U_INT_8_ARRAY:
+        return content
+      case user.Message.Type.STRING:
+        return textDecoder.decode(content)
+      default:
+        throw new Error('Unknown message type')
       }
     }
     return content
   }
 
   /**
-   * Identify the user message type.
-   *
-   * @private
-   * @param {Message} data User message
-   * @returns {MessageTypeEnum} User message type
+   * Identify the user data type.
    */
-  userDataToType (data) {
+  userDataToType (data): { type: number , bytes: Uint8Array } {
     if (data instanceof Uint8Array) {
       return { type: user.Message.Type.U_INT_8_ARRAY, bytes: data }
     } else if (typeof data === 'string' || data instanceof String) {
       return { type: user.Message.Type.STRING, bytes: textEncoder.encode(data) }
     } else {
-      throw new Error('Message neigther a string or a Uint8Array object')
+      throw new Error('Message neigther a string type or a Uint8Array type')
     }
   }
 
-  getBuffer (peerId, msgId) {
+  getBuffer (peerId: number, msgId: number): Buffer {
     const buffers = this.buffers.get(peerId)
     if (buffers !== undefined) {
       return buffers.get(msgId)
@@ -134,7 +119,7 @@ export class UserMessage {
     return undefined
   }
 
-  setBuffer (peerId, msgId, buffer) {
+  setBuffer (peerId: number, msgId: number, buffer: Buffer): void {
     let buffers = this.buffers.get(peerId)
     if (buffers === undefined) {
       buffers = new Map()
@@ -149,17 +134,13 @@ export class UserMessage {
  * may be sent over a `Channel`. Each buffer is identified by `WebChannel` id,
  * peer id (who sends the big message) and message id (in case if the peer sends
  * more then 1 big message at a time).
- * @private
  */
 class Buffer {
-  /**
-   * @param {number} totalLength The total user message size
-   * @param {ArrayBuffer} data The first chunk of the user message
-   * @param {number} chunkNb Number of the chunk
-   * @param {function(buffer: ArrayBuffer)} action Callback to be executed when all
-   * message chunks are received and thus the message is ready
-   */
-  constructor (totalLength, data, chunkNb) {
+
+  private fullData: Uint8Array
+  private currentLength: number
+
+  constructor (totalLength: number, data: Uint8Array, chunkNb: number) {
     this.fullData = new Uint8Array(totalLength)
     this.currentLength = 0
     this.append(data, chunkNb)
@@ -167,11 +148,8 @@ class Buffer {
 
   /**
    * Add a chunk of message to the buffer.
-   * @param {ArrayBuffer} data - Message chunk
-   * @param {number} chunkNb - Number of the chunk
-   * @return {undefined|ArrayBuffer}
    */
-  append (data, chunkNb) {
+  append (data: Uint8Array, chunkNb: number): Uint8Array | undefined {
     let i = chunkNb * MAX_USER_MSG_SIZE
     this.currentLength += data.length
     for (let d of data) {
