308d95b6df3fb8a5c76025ce667047f59ed7fc9c
Philippe Kalitine <philippe.kalitine@gmail.com>
2018-05-17 13:26:17 +0200

    fix(typings): include declarations for TextEncoder/TextDecoder API for TypeScript < 2.8

--- src/misc/typings.d.ts
+++ src/misc/typings.d.ts
@@ -12,6 +12,45 @@ declare var global: Window & {
   fullmesh: any // for debuging
 }
 
+// The following Text Encoding declaration are added because they exist only started from TypeScript v2.8.x
+interface TextDecoder {
+  readonly encoding: string
+  readonly fatal: boolean
+  readonly ignoreBOM: boolean
+  decode(
+    input?:
+      | Int8Array
+      | Int16Array
+      | Int32Array
+      | Uint8Array
+      | Uint16Array
+      | Uint32Array
+      | Uint8ClampedArray
+      | Float32Array
+      | Float64Array
+      | DataView
+      | ArrayBuffer
+      | null,
+    options?: TextDecodeOptions
+  ): string
+}
+
+declare var TextDecoder: {
+  prototype: TextDecoder
+  new (label?: string, options?: TextDecoderOptions): TextDecoder
+}
+
+interface TextEncoder {
+  readonly encoding: string
+  encode(input?: string): Uint8Array
+}
+
+declare var TextEncoder: {
+  prototype: TextEncoder
+  new (): TextEncoder
+}
+
+// Types for NodeJS environment
 declare var require: (id: string) => any
 declare type NodeJSHttpServer = any // NodeJS http.Server
 declare type NodeJSHttpsServer = any // NodeJS https.Server
